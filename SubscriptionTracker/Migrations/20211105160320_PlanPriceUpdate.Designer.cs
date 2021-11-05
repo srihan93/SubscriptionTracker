4efa200d4696bcb1ed95831229bea1dca5dd6c3f
srihan93 <srihan93@gmail.com>
2021-11-05 21:50:06 +0530

    fix for plan price in all pages

--- SubscriptionTracker/Migrations/20211105160320_PlanPriceUpdate.Designer.cs
+++ SubscriptionTracker/Migrations/20211105160320_PlanPriceUpdate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SubscriptionTracker.Models;
 
 namespace SubscriptionTracker.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211105160320_PlanPriceUpdate")]
+    partial class PlanPriceUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SubscriptionTracker/Migrations/20211105160320_PlanPriceUpdate.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SubscriptionTracker.Migrations
+{
+    public partial class PlanPriceUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Plans",
+                keyColumn: "PlanId",
+                keyValue: 2,
+                column: "Cost",
+                value: 2800m);
+
+            migrationBuilder.UpdateData(
+                table: "Plans",
+                keyColumn: "PlanId",
+                keyValue: 3,
+                column: "Cost",
+                value: 4000m);
+
+            migrationBuilder.UpdateData(
+                table: "Plans",
+                keyColumn: "PlanId",
+                keyValue: 4,
+                column: "Cost",
+                value: 6500m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Plans",
+                keyColumn: "PlanId",
+                keyValue: 2,
+                column: "Cost",
+                value: 1200m);
+
+            migrationBuilder.UpdateData(
+                table: "Plans",
+                keyColumn: "PlanId",
+                keyValue: 3,
+                column: "Cost",
+                value: 1200m);
+
+            migrationBuilder.UpdateData(
+                table: "Plans",
+                keyColumn: "PlanId",
+                keyValue: 4,
+                column: "Cost",
+                value: 1200m);
+        }
+    }
+}
